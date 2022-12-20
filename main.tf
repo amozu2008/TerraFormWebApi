3e7a740e5439e194e28391e01b7246f40c15a5ec
Oguejiofor <jude.oguejiofor@capgemini.com>
2022-12-20 09:16:47 +0100

    added main.tf

---
+++ main.tf
@@ -0,0 +1,41 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
+  }
+
+  required_version = ">= 1.1.0"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "tf_test" {
+    name = "terraFormWebApi-rg"
+    location = "West Europe"
+  
+}
+
+resource "azurerm_container_group" "tf_container_test" {
+  name = "terraFormWebApi-container-rg"
+  location = azurerm_resource_group.tf_test.location
+  resource_group_name = azurerm_resource_group.tf_test.name
+  ip_address_type = "Public"
+  os_type = "Linux"
+  dns_name_label = "terraFormWebApi-dns-label"
+
+  container {
+    name = "joguejio-container"
+    image = "judeoguejiofor927/terraformwebapi"
+    cpu = 1
+    memory = 1
+    ports {
+      port = 80
+      protocol = "TCP"
+    }
+  }
+
+}
